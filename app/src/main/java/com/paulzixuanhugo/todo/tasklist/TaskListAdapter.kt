ee6e7b45f0bc03b5ff7cc74b838935345466365d
hugo <hugo.caroubalos@telecom-sudparis.eu>
2020-11-23 18:17:53 +0100

    Fin du TD2 en distanciel

---
+++ app/src/main/java/com/paulzixuanhugo/todo/tasklist/TaskListAdapter.kt
@@ -0,0 +1,35 @@
+package com.paulzixuanhugo.todo.tasklist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.paulzixuanhugo.todo.R
+
+class TaskListAdapter(private val taskList: List<String>) :
+    RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
+
+    inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(taskTitle: String) {
+            itemView.apply { // `apply {}` permet d'éviter de répéter `itemView.*`
+                val textView  = itemView.findViewById<TextView>(R.id.task_title)
+                //val resultText: TextView = findViewById(R.id.textView)
+                textView.text=taskTitle
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
+        return TaskViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.bind(taskList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return taskList.size
+    }
+}
---
+++ app/src/main/java/com/paulzixuanhugo/todo/tasklist/TaskListFragment.kt
@@ -0,0 +1,33 @@
+package com.paulzixuanhugo.todo.tasklist
+
+import android.os.Bundle
+import android.os.Debug
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.paulzixuanhugo.todo.R
+
+class TaskListFragment : Fragment() {
+
+    private val taskList = listOf("Task 1", "Task 2", "Task 3")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_task_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.adapter = TaskListAdapter(taskList)
+    }
+}
