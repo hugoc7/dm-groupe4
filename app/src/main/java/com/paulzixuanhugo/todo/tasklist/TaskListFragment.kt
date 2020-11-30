6ac10b67eb697acfab4dba856441433b4ccf4c23
paul ciupek <paul.ciupek@telecom-sudparis.eu>
2020-11-30 17:53:54 +0100

    Fin TP2

--- app/src/main/java/com/paulzixuanhugo/todo/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/paulzixuanhugo/todo/tasklist/TaskListFragment.kt
@@ -9,11 +9,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.paulzixuanhugo.todo.R
+import java.util.*
 
 class TaskListFragment : Fragment() {
 
-    private val taskList = listOf(
+    private val taskList = mutableListOf(
             Task(id = "id_1", title = "Task 1", description = "description 1"),
             Task(id = "id_2", title = "Task 2"),
             Task(id = "id_3", title = "Task 3")
@@ -33,5 +35,12 @@ class TaskListFragment : Fragment() {
         val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(activity)
         recyclerView.adapter = TaskListAdapter(taskList)
+
+        val fab = view.findViewById<FloatingActionButton>(R.id.floatingActionButton2)
+        fab.setOnClickListener{
+            val newTask = Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}")
+            taskList.add(newTask)
+            TaskListAdapter(taskList).notifyDataSetChanged()
+        }
     }
 }
