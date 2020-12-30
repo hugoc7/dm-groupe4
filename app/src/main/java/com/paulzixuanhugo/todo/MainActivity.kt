79ff64285e9b86782de4d66278e440f0d8b2dc0e
paul ciupek <paul.ciupek@telecom-sudparis.eu>
2020-12-30 21:57:37 +0100

    Final touch

--- app/src/main/java/com/paulzixuanhugo/todo/MainActivity.kt
+++ app/src/main/java/com/paulzixuanhugo/todo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.paulzixuanhugo.todo
 
+import android.app.PendingIntent.getActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,5 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setTitle("Tasklist app by Paul Ciupek & Hugo Caroubalos");
     }
 }
