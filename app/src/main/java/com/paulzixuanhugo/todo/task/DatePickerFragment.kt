3a8f6412a9582955f12dcf5811b05b64bc1b5d54
hugo <hugo.caroubalos@telecom-sudparis.eu>
2021-01-01 18:07:18 +0100

    TD8: ajout du champ dueDate dans les todos (reseau + affichage et edition)

---
+++ app/src/main/java/com/paulzixuanhugo/todo/task/DatePickerFragment.kt
@@ -0,0 +1,29 @@
+package com.paulzixuanhugo.todo.task
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+import java.util.*
+
+class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    // Use the current date as the default date in the picker
+    private val c = Calendar.getInstance()
+    var year = c.get(Calendar.YEAR)
+    var month = c.get(Calendar.MONTH)
+    var day = c.get(Calendar.DAY_OF_MONTH)
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // Create a new instance of DatePickerDialog and return it
+        return DatePickerDialog(requireContext(), this, year, month, day)
+    }
+
+    override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
+        // Do something with the date chosen by the user
+        onDateSetListener?.invoke(day, month, year)
+    }
+
+    var onDateSetListener: ((Int, Int, Int) -> Unit)? = null
+}
